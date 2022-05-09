ff7b2dd47e3b164738e57eacd4359b5c302db37b
ChristopherSemard <christophersemard76@gmail.com>
2022-05-09 16:52:22 +0200

    fix

--- app.js
+++ app.js
@@ -7,14 +7,13 @@ buttonSwitchMode.addEventListener("click", ()=>{
     document.documentElement.setAttribute("data-theme", newTheme);
 })
 
-// Filtre
-
+// Ecoute des boutons filtres
 buttonsFilter = document.querySelectorAll('main nav button')
 console.log(buttonsFilter);
 for(button of buttonsFilter){
     button.addEventListener("click", (e) => {
-
         switchButton(e.target)
+
         // ID de la catégorie cliquée
         idFilter = e.target.id
         console.log(idFilter);
@@ -25,7 +24,7 @@ for(button of buttonsFilter){
 
 // Enlever la classe active de tous les boutons et la rajouter à celui cliqué
 function switchButton(buttonActive) {
-        for(buttonInactive of buttonsFilter){
+        for(let buttonInactive of buttonsFilter){
             buttonInactive.classList.remove('active');
         }
         buttonActive.classList.add('active');
